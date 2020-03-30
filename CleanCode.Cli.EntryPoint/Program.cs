de8803f5e96716c2122a3361b9e395e17ced2f63
apetryashov <mopozko123@gmail.com>
2020-03-30 16:34:34 +0500

    add new TODO issues

--- CleanCode.Cli.EntryPoint/Program.cs
+++ CleanCode.Cli.EntryPoint/Program.cs
@@ -6,6 +6,9 @@ namespace CleanCode.Tool
     //TODO: Затащить это все в chocolatey
     //TODO: Добавить команду generate-dot-settings
     //TODO: Разделить логику и логирование
+    //TODO: Добавить тесты
+    //TODO: Перевести на DI (будет полезно для тестов)
+    //TODO: Добавить автооткрытие репорта после завершения
     internal static class Program
     {
         private static void Main(string[] args)
